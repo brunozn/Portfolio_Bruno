cd097910d6946faec98f482c3e2c492672471558
Bruno José <46513538+brunozn@users.noreply.github.com>
2020-06-08 07:29:40 -0300

    Ocultando o navbar quando a tela fica com uma resolução menor

--- public/js/index.js
+++ public/js/index.js
@@ -10,3 +10,20 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+function typeWrite(elemento, time) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => {
+        elemento.innerHTML += letra;
+        }, time * i)
+        console.log(letra);
+    });
+}
+
+//const title1 = document.querySelector(".section__title");
+const title = document.querySelector(".hello");
+const subtitle = document.querySelector(".strong");
+typeWrite(title, 60);
+typeWrite(subtitle, 110);
