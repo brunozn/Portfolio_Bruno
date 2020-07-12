b662ed70ec95f5bc6ca4b12d529f4a83b6d8999e
brunozn <brunojndias@gmail.com>
2020-07-12 18:45:37 -0300

    Melhorias nas seçoes

--- public/js/index.js
+++ public/js/index.js
@@ -27,3 +27,16 @@ const title = document.querySelector(".hello");
 const subtitle = document.querySelector(".strong");
 typeWrite(title, 60);
 typeWrite(subtitle, 110);
+
+const modalBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', function(){
+    modal.classList.add('open-modal')
+})
+
+closeBtn.addEventListener('click', function(){
+    modal.classList.remove('open-modal')
+})
+
