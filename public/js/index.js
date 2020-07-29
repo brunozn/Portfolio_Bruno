7428d3a39f0c35c85006001c2e5b4a280eed12c5
brunozn <brunojndias@gmail.com>
2020-07-29 04:34:50 -0300

    Ajustando as imagens da section my work

--- public/js/index.js
+++ public/js/index.js
@@ -1,42 +1,23 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
 
 function typeWrite(elemento, time) {
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    textoArray.forEach((letra, i) => {
-        setTimeout(() => {
-        elemento.innerHTML += letra;
-        }, time * i)
-        console.log(letra);
-    });
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => {
+      elemento.innerHTML += letra;
+    }, time * i);
+    console.log(letra);
+  });
 }
-
-//const title1 = document.querySelector(".section__title");
-const title = document.querySelector(".hello");
-const subtitle = document.querySelector(".strong");
-typeWrite(title, 60);
-typeWrite(subtitle, 110);
-
-const modalBtn = document.querySelector('.modal-btn');
-const modal = document.querySelector('.modal-overlay');
-const closeBtn = document.querySelector('.close-btn');
-
-modalBtn.addEventListener('click', function(){
-    modal.classList.add('open-modal')
-})
-
-closeBtn.addEventListener('click', function(){
-    modal.classList.remove('open-modal')
-})
-
