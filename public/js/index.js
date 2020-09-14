4a0d1c7d05acacbbca98613fac46001aa1b121e6
brunozn <brunojndias@gmail.com>
2020-09-14 11:13:47 -0300

    prettier cod

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,19 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav__link");
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
 });
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
   });
 });
 
 function typeWrite(elemento, time) {
-  const textoArray = elemento.innerHTML.split("");
-  elemento.innerHTML = "";
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
   textoArray.forEach((letra, i) => {
     setTimeout(() => {
       elemento.innerHTML += letra;
